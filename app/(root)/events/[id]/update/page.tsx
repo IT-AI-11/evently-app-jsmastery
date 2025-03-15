6b13f3686e93fbbbe8b64bb24c4379f8029454f2
Nextjs <tech.united.it@gmail.com>
2025-03-15 11:42:30 +0200

    trying to resolve error 3

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,30 +1,30 @@
+// ЗАКРАЛ для debugging
 
 
 
 
-
-import EventForm from "@/components/shared/EventForm"
-//import { auth } from "@clerk/nextjs";    original
-import { auth } from "@clerk/nextjs/server";
-
+// import EventForm from "@/components/shared/EventForm"
+// //import { auth } from "@clerk/nextjs";    original
+// import { auth } from "@clerk/nextjs/server";
 
 
 
-export default async function UpdateEvent () {
 
-  const { sessionClaims } = await auth();
+// export default async function UpdateEvent () {
 
-  const userId = sessionClaims?.userId as string;
+//   const { sessionClaims } = await auth();
 
-  return (
-    <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">Update Event</h3>
-      </section>
+//   const userId = sessionClaims?.userId as string;
 
-      <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" />
-      </div>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+//         <h3 className="wrapper h3-bold text-center sm:text-left">Update Event</h3>
+//       </section>
+
+//       <div className="wrapper my-8">
+//         <EventForm userId={userId} type="Update" />
+//       </div>
+//     </>
+//   )
+// }
